a5b2e4e805b60d900b4e094dd838a806a31dd01a
Stephen Gray <sgray@miletwo.us>
2021-11-12 11:35:04 -0500

    Added a build script

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,35 @@
-import "reflect-metadata";
-import { buildSchema } from "type-graphql";
-import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { PrismaClient } from "@prisma/client";
-import { resolvers } from "../prisma/generated/type-graphql";
-import path from "path";
+import 'reflect-metadata'
+import { buildSchema } from 'type-graphql'
+import { ApolloServer } from 'apollo-server'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { PrismaClient } from '@prisma/client'
+import { resolvers } from '../prisma/generated/type-graphql'
+import path from 'path'
 
 interface Context {
-  prisma: PrismaClient;
+  prisma: PrismaClient
 }
 
 async function main() {
   const schema = await buildSchema({
     resolvers,
-    emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql"),
+    emitSchemaFile: path.resolve(__dirname, './generated-schema.graphql'),
     validate: false,
-  });
+  })
 
   const prisma = new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  });
-  await prisma.$connect();
+    log: ['query', 'info', 'warn', 'error'],
+  })
+  await prisma.$connect()
 
   const server = new ApolloServer({
     schema,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     context: (): Context => ({ prisma }),
-  });
-  const { port } = await server.listen(process.env.APOLLO_SERVER_PORT || 4000);
-  console.log(
-    `GraphQL Server is listening on http://localhost:${port}/graphql`
-  );
+    healthCheckPath: '/api/health',
+  })
+  const { port } = await server.listen(process.env.APOLLO_SERVER_PORT || 4000)
+  console.log(`GraphQL Server is listening on http://localhost:${port}/graphql`)
 }
 
-main().catch(console.error);
+main().catch(console.error)
